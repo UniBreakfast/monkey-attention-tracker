ab799fab5a382850205dd444371652d067f4efa2
unibreakfast <unibreakfast1@gmail.com>
2020-04-23 08:06:24 +0300

    recalc, multiline, chartByX

--- ctx.methods.js
+++ ctx.methods.js
@@ -7,6 +7,21 @@ Ctx.prototype.coordinateAxes = function (x, y, width, height) {
         yLength = height - arrowWidth/2
   this.arrowUp(x0, y0, yLength, arrowLength, arrowWidth)
   this.arrowRight(x0, y0, xLength, arrowWidth, arrowLength)
+
+
+  function recalc(points) {
+    const xSize = Math.max(...points.map(point => point[0])),
+          ySize = Math.max(...points.map(point => point[1])),
+          xSpace = xLength - arrowLength - 7,
+          ySpace = yLength - arrowLength - 7,
+          xStep = xSpace / xSize,
+          yStep = ySpace / ySize
+    return points.map(([x, y])=> [x0+1 + x*xStep, y0-1 - y*yStep])
+  }
+
+  recalc.chartByX = points => this.chartByX(recalc(points))
+
+  return recalc
 }
 
 Ctx.prototype.triangle = function (x1, y1, x2, y2, x3, y3) {
@@ -37,6 +52,25 @@ Ctx.prototype.arrowRight = function (x, y, length, height, width) {
   this.lineTo(x1-width, y)
   this.stroke()
   this.triangle(x1, y1, x2, y2, x3, y3)
+}
+
+Ctx.prototype.multiline = function (points) {
+  this.beginPath()
+  points.forEach((point, i)=> this[i? 'lineTo':'moveTo'](...point))
+  this.stroke()
+}
+
+Ctx.prototype.chartByX = function (points) {
+  this.beginPath()
+  points.forEach(([x, y], i)=> {
+    if (i) {
+      const [x0, y0] = points[i-1]
+      const step = x - x0
+      const shift = step/2, shift0 = i-1 && shift
+      this.bezierCurveTo(x0 + shift0, y0, x - shift, y, x, y)
+    } else this.moveTo(x, y)
+  })
+  this.stroke()
 }
 
 Ctx.prototype.lines = function (x, y, width, height, num=2) {
--- script.js
+++ script.js
@@ -10,3 +10,14 @@ ctx.fillRect(0, 0, 800, 600)
 // const graph = ctx.drawCoordinateGrid(50, 50, 700, 500, -3, 10, -6, 4)
 
 ctx.fillStyle = "black"
+
+
+
+
+// const recalc = ctx.coordinateAxes(50, 50, 700, 500)
+// ctx.chartByX(recalc([[0, 0], [1, 1], [2, 2], [3, 7], [4, 3], [5, 20]]))
+
+ctx.setLineDash([5, 10])
+
+ctx.coordinateAxes(50, 50, 700, 500)
+   .chartByX([[0, 0], [1, 10], [2, 2], [3, 7], [4, 3], [13, 100]])
